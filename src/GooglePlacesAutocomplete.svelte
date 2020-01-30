c89a46426ad2584f4ca36bfc941b278d1397223e
Matt Henderson <matt_henderson@sil.org>
2020-01-30 14:01:59 -0500

    Expose place_changed event with placeResult data

--- src/GooglePlacesAutocomplete.svelte
+++ src/GooglePlacesAutocomplete.svelte
@@ -1,13 +1,21 @@
 <script>
 import { loadGooglePlacesLibrary } from './loader.js'
+import { createEventDispatcher } from 'svelte'
 
 export let apiKey
 export let options = undefined
+
+const dispatch = createEventDispatcher()
 
 let inputField
 
 loadGooglePlacesLibrary(apiKey, () => {
   const autocomplete = new google.maps.places.Autocomplete(inputField, options)
+  autocomplete.addListener('place_changed', event => {
+    dispatch('place_changed', {
+      placeResult: autocomplete.getPlace()
+    })
+  })
 })
 </script>
 
