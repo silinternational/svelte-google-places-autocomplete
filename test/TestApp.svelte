56dad393423c48fdf6f9c1654fda0ca8f1be1d6a
Matt Henderson <matt_henderson@sil.org>
2020-03-17 14:13:18 -0400

    Set up an initial test

--- test/TestApp.svelte
+++ test/TestApp.svelte
@@ -6,18 +6,87 @@ const options = {
   types: ['(cities)']
 }
 
+let displayText = 'Please enter your Google Places API Key'
 let googlePlacesApiKey
+let onPlaceChangeCallbacks = []
+let testPasses = null
 
 function onApiKeyProvided(event) {
   googlePlacesApiKey = event.target.value
+  showText('Please wait')
+}
+
+function onPlaceChanged(event) {
+  while (onPlaceChangeCallbacks.length) {
+    onPlaceChangeCallbacks.pop()(event.detail)
+  }
+}
+
+async function runTests() {
+  const input = document.querySelector('input')
+  input.focus()
+  
+  try {
+    await waitAMoment(750);
+    await type(input, 'new')
+    showText('Please click on the first suggestion')
+    whenPlaceChanges(() => {
+      testPasses = (input.value === 'New York, NY, USA')
+      clearText()
+    })
+  } catch (e) {
+    testPasses = false
+    showText(e.message)
+    input.blur()
+  }
+}
+
+async function waitAMoment(milliseconds = 100) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+async function type(input, letters) {
+  for (let i = 0; i < letters.length; i++) {
+    await typeLetter(input, letters[i])
+  }
+}
+
+async function typeLetter(input, letter) {
+  return new Promise(resolve => {
+    const simulatedEvent = new Event('input', {
+      bubbles: true,
+      cancelable: true,
+      data: letter,
+    })
+    input.value += letter
+    input.dispatchEvent(simulatedEvent)
+    waitAMoment().then(resolve)
+  })
+}
+
+function showText(text) {
+  displayText = text
+}
+
+function clearText() {
+  displayText = ''
+}
+
+function whenPlaceChanges(callback) {
+  onPlaceChangeCallbacks.push(callback)
 }
 </script>
 
+<p>{ displayText }</p>
+
+{#if testPasses !== null}
+  <p>Test result: { testPasses ? 'PASS' : 'FAIL' }</p>
+{/if}
+
 {#if googlePlacesApiKey}
-  <GooglePlacesAutocomplete apiKey={googlePlacesApiKey} {options} />
+  <GooglePlacesAutocomplete apiKey={googlePlacesApiKey} {options}
+                            on:place_changed={onPlaceChanged}
+                            on:ready={runTests} />
 {:else}
-  <label>
-    <p>Google Places API Key:</p>
-    <input on:change={onApiKeyProvided} />
-  </label>
+  <input on:change={onApiKeyProvided} />
 {/if}
