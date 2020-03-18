5bea2759b7ab1542d674ec5d10f6cad72350f01d
Matt Henderson <matt_henderson@sil.org>
2020-03-18 15:43:09 -0400

    Move test 1 to an entry in an array

--- test/TestApp.svelte
+++ test/TestApp.svelte
@@ -7,16 +7,24 @@ const options = {
 }
 
 let displayText = 'Please enter your Google Places API Key'
+let error
 let googlePlacesApiKey
 let locationInput
 let onPlaceChangeCallbacks = []
 let testPasses = null
+const tests = [
+  {
+    name: `Type something, see suggestions, click on one`,
+    setup: async () => await type('new').then(waitForSuggestions),
+    go: () => showText('Please click on the first suggestion'),
+    passed: () => locationInput.value === 'New York, NY, USA',
+  },
+]
 
 window.addEventListener("unhandledrejection", event => {
   event.preventDefault();
   
-  testPasses = false
-  showText(event.reason)
+  error = event.reason
   locationInput.blur()
 }, false);
 
@@ -34,27 +42,32 @@ async function runTests() {
   locationInput = document.querySelector('input')
   locationInput.focus()
   
-  await waitAMoment();
-  await runTest1()
-  await runTest2()
-  await runTest3()
+  await waitAMoment(1000)
+  for (let i = 0; i < tests.length; i++) {
+    let test = tests[i]
+    try {
+      await runTest(test)
+      test.result = 'pass'
+    } catch (e) {
+      test.result = 'fail'
+      test.details = e.message
+      break
+    } finally {
+      tests[i] = test
+    }
+  }
 }
 
-async function runTest1() {
-  return new Promise(async resolve => {
-    resetForNextTest()
-    
-    showText('Please wait')
-    await type('new')
-    await waitForSuggestions()
+async function runTest(test) {
+  return new Promise(async (resolve, reject) => {
+    await test.setup()
     
     whenPlaceChanges(() => {
-      testPasses = (locationInput.value === 'New York, NY, USA')
       clearText()
-      resolve()
+      test.passed() ? resolve() : reject()
     })
     
-    showText('Please click on the first suggestion')
+    test.go()
   })
 }
 
@@ -104,6 +117,7 @@ async function waitAMoment(milliseconds = 100) {
 }
 
 async function type(letters) {
+  showText('Please wait')
   for (let i = 0; i < letters.length; i++) {
     await typeLetter(letters[i])
   }
@@ -145,7 +159,7 @@ async function waitForSuggestions() {
   return new Promise(async (resolve, reject) => {
     const timeoutHandle = setTimeout(
       () => reject('Timed out waiting for suggestions to appear'),
-      2500
+      5000
     )
     let suggestion = document.querySelector('.pac-item')
     while (!suggestion) {
@@ -162,16 +176,65 @@ function whenPlaceChanges(callback) {
 }
 </script>
 
-<p>{ displayText }</p>
+<style>
+body,
+html {
+  height: 100%;
+}
 
-{#if testPasses !== null}
-  <p>Test result: { testPasses ? 'PASS' : 'FAIL' }</p>
+.error,
+.fail {
+  color: red;
+}
+.pass {
+  color: green;
+}
+
+th {
+  text-align: left;
+}
+
+.uppercase {
+  text-transform: uppercase;
+}
+
+.ui {
+  height: 100px;
+  margin-bottom: 20px;
+}
+</style>
+
+{#if error }
+  <h2 class="error">{ error }</h2>
 {/if}
 
-{#if googlePlacesApiKey}
-  <GooglePlacesAutocomplete apiKey={googlePlacesApiKey} {options}
-                            on:place_changed={onPlaceChanged}
-                            on:ready={runTests} />
-{:else}
-  <input on:change={onApiKeyProvided} />
-{/if}
+<div class="ui">
+  <p>{ displayText }</p>
+  
+  {#if googlePlacesApiKey}
+    <GooglePlacesAutocomplete apiKey={googlePlacesApiKey} {options}
+                              on:place_changed={onPlaceChanged}
+                              on:ready={runTests} />
+  {:else}
+    <input on:change={onApiKeyProvided} />
+  {/if}
+</div>
+
+<table style="width: 100%">
+  <thead>
+    <tr>
+      <th>Test</th>
+      <th>Result</th>
+      <th>Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      {#each tests as test (test.name) }
+        <td style="width: 45%">{ test.name }</td>
+        <td style="width: 10%" class="uppercase {test.result || ''}">{ test.result || '' }</td>
+        <td style="width: 45%">{ test.details || '' }</td>
+      {/each}
+    </tr>
+  </tbody>
+</table>
