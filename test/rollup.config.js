db397eae8348d59e84bc5fc30c591ee6f1b37849
Matt Henderson <matt_henderson@sil.org>
2020-03-19 11:38:39 -0400

    Replace tabs with spaces in rollup.config.js

--- test/rollup.config.js
+++ test/rollup.config.js
@@ -3,16 +3,16 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from "rollup-plugin-livereload";
 
 export default {
-	input: 'test/test-main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: `test/test-bundle.js`
-	},
-	plugins: [
-		svelte(),
-		resolve(),
-		livereload('test'),
-	]
+  input: 'test/test-main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: `test/test-bundle.js`
+  },
+  plugins: [
+    svelte(),
+    resolve(),
+    livereload('test'),
+  ]
 };
